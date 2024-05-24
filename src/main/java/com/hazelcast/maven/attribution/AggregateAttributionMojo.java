72b6f4faa51f692511a1070966df07e6b565aac5
Josef Cacek <josef.cacek@gmail.com>
2024-05-24 15:48:04 +0200

    dev

--- src/main/java/com/hazelcast/maven/attribution/AggregateAttributionMojo.java
+++ src/main/java/com/hazelcast/maven/attribution/AggregateAttributionMojo.java
@@ -1,12 +1,5 @@
 package com.hazelcast.maven.attribution;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -14,11 +7,20 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuildingRequest;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Generates an aggregated attribution file for a (possibly) multi-module project.
  */
-@Mojo(name = "aggregate", defaultPhase = LifecyclePhase.PACKAGE, aggregator = true, inheritByDefault = false,
-    requiresDependencyCollection = ResolutionScope.RUNTIME, threadSafe = true)
+@Mojo(name = "aggregate", defaultPhase = LifecyclePhase.PROCESS_RESOURCES,
+        aggregator = true, requiresDependencyCollection = ResolutionScope.RUNTIME,
+        requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
+        threadSafe = true)
 public class AggregateAttributionMojo extends AbstractAttributionMojo {
 
     @Override
@@ -33,32 +35,31 @@ public class AggregateAttributionMojo extends AbstractAttributionMojo {
                 projectGaSet.add(projectGaKey);
             }
             for (final MavenProject p : reactorProjects) {
-//                List<Artifact> projectArtifacts = p.getRuntimeArtifacts();
-//                getLog().debug("Project " + gaKey(p.getGroupId(), p.getArtifactId()) + " artifacts: " + projectArtifacts);
-//
-//                ProjectBuildingRequest pbr = getProjectBuildingRequest(p);
-//                for (Artifact artifact : projectArtifacts) {
-//                    String gaKey = gaKey(artifact);
-//                    String gavKey = gavKey(artifact);
-//                    if (artifacts.contains(gavKey)) {
-//                        getLog().debug("Artifact already resolved " + gavKey);
-//                        continue;
-//                    }
-//                    artifacts.add(gavKey);
-//                    if (projectGaSet.contains(gaKey)) {
-//                        // don't include (sub)project sources
-//                        getLog().debug("Skipping (sub)project artifact " + gaKey);
-//                        continue;
-//                    }
-//                    File sourceFile = resourceResolver.resolveSourceFromArtifact(pbr, p, artifact);
-//                    if (sourceFile == null) {
-//                        getLog().debug("No source file resolved for " + gavKey);
-//                        continue;
-//                    }
-//                    getLog().debug("Resolved " + sourceFile);
-//                    result.put(gavKey, sourceFile);
-//                }
+                List<Artifact> projectArtifacts = p.getRuntimeArtifacts();
+                getLog().debug("Project " + gaKey(p.getGroupId(), p.getArtifactId()) + " artifacts: " + projectArtifacts);
 
+                ProjectBuildingRequest pbr = getProjectBuildingRequest(p);
+                for (Artifact artifact : projectArtifacts) {
+                    String gaKey = gaKey(artifact);
+                    String gavKey = gavKey(artifact);
+                    if (artifacts.contains(gavKey)) {
+                        getLog().debug("Artifact already resolved " + gavKey);
+                        continue;
+                    }
+                    artifacts.add(gavKey);
+                    if (projectGaSet.contains(gaKey)) {
+                        // don't include (sub)project sources
+                        getLog().debug("Skipping (sub)project artifact " + gaKey);
+                        continue;
+                    }
+                    File sourceFile = resourceResolver.resolveSourceFromArtifact(pbr, p, artifact);
+                    if (sourceFile == null) {
+                        getLog().debug("No source file resolved for " + gavKey);
+                        continue;
+                    }
+                    getLog().debug("Resolved " + sourceFile);
+                    result.put(gavKey, sourceFile);
+                }
             }
         } else {
             getLog().info("Null reactorProjects");
